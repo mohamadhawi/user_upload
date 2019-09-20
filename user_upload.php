9d7a4b38174bb347130e83f9572ad4a22226f22f
Mohammad Hawi <34213886+mohamadhawi@users.noreply.github.com>
2019-09-21 09:36:44 +1000

    Update user_upload.php
    PHP 7 tested

--- user_upload.php
+++ user_upload.php
@@ -1,33 +1,25 @@
 <?php
-error_reporting(1);
 
 function arguments($argv) {
-    $_ARG = array();
-    foreach ($argv as $arg) {
-      if (ereg('--([^=]+)=(.*)',$arg,$reg)) {
-        $_ARG[$reg[1]] = $reg[2];
-
-      } 
-	  
-	  elseif (ereg('-([^=]+)=(.*)',$arg,$reg)) {
-        $_ARG[$reg[1]] = $reg[2];
-
-      } 
-	  
-	  elseif(ereg('--([^=]+)',$arg,$reg)) {
-            $_ARG[$reg[1]] = 'true';
-        }
-  
+$_ARG = array();
+$seperator = array("-","--");
+foreach ($argv as $arg) {
+         $e=explode("=",$arg);
+        if(count($e)==2)
+            $_ARG[str_replace($seperator,'',$e[0])]=$e[1];
+        else {  
+            $e=explode("--",$arg);
+			if(count($e)==2)
+			$_ARG[$e[1]]=$e[1];
+		}
     }
-  return $_ARG;
+return $_ARG;
 }
 
 $arr = arguments($argv);
+//var_dump($argv);
 
-
-$help = $arr["help"];
-
-if ($help) {
+if (array_key_exists("help",$arr)) {
 	echo "my help goes here";
 	exit;
 }
@@ -38,6 +30,7 @@ $conn = new mysqli($arr['h'], $arr['u'], $arr['p']);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+	exit;
 }
 // Create database
 $sql = "CREATE DATABASE IF NOT EXISTS users";
@@ -45,6 +38,7 @@ if ($conn->query($sql) === TRUE) {
     echo "Database created successfully\n";
 } else {
     echo "Error creating database: \n" . $conn->error . "\n";
+	exit;
 }
 
 
@@ -54,6 +48,7 @@ $link = mysqli_connect($arr['h'], $arr['u'], $arr['p'], "users");
 
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
+	exit;
 }
 
 $sql = "CREATE Table IF NOT EXISTS users(
@@ -66,43 +61,58 @@ if(mysqli_query($link, $sql)){
     echo "Table created successfully.\n";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link) . "\n";
+	exit;
 }
  
-if ($arr["create_table"])
-exit;
+if (array_key_exists("create_table",$arr)) {
+$sql = "TRUNCATE TABLE users";
 
+if(mysqli_query($link, $sql)){
+    echo "No data imported.\n";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link) . "\n";
+}
+exit;	
+}
 
+if (!array_key_exists("file",$arr)) {
+	echo "ERROR: No file parameter\n";
+	exit;
+}
 
 
 $fileName = $arr["file"];
 
+
+
 $file = fopen($fileName, "r");
+$message="";
 
-$dry_run = $arr["dry_run"];
+if (!array_key_exists("dry_run",$arr)) {
+$flag = true; 
+$dup=0;
+$r=0;
+$inv=0;
 
-if (!$dry_run) {
-$flag = true;      
 while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
 	
 	if($flag) { $flag = false; continue; }
 	
-	$sqlInsert = "INSERT into users (name,surname,email)
-		   values ('" . $column[0] . "','" . $column[1] . "','" . $column[2] . "')";
-	$result = mysqli_query($link, $sqlInsert);
 	
-	if (! empty($result)) {
-		$type = "success";
-		$message = "CSV Data Imported into the Database\n";
-	} else {
-		$type = "error";
-		$message = "Problem in Importing CSV Data\n";
+	if (filter_var( trim(addslashes($column[2])), FILTER_VALIDATE_EMAIL)) {
+    $sqlInsert = "INSERT into users (name,surname,email)
+		   values ('" . ucfirst(strtolower(addslashes($column[0]))) . "','" . ucfirst(strtolower(addslashes($column[1]))) . "','" . strtolower(addslashes($column[2])) . "')";
+	$result = mysqli_query($link, $sqlInsert);
+	if ($result) $r++; else $dup++;
 	}
+	else $inv++;
+	
 }
+	$message = $r." rows data imported into the database\n".$inv." rows invalid email\n".$dup." rows duplicate email @\n" ;
+	
 }
   // Close connection
 $link->close();  
-mysqli_close($link);  
+ 
 echo $message;
-echo $type;
-
 ?>
