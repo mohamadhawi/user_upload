ca63d2937207268126b721bf9329dcd16da03773
Mohammad Hawi <34213886+mohamadhawi@users.noreply.github.com>
2019-09-22 06:24:42 +1000

    Update user_upload.php

--- user_upload.php
+++ user_upload.php
@@ -1,8 +1,20 @@
 <?php
+/**
+ * user_upload
+ *
+ * Import CSV file of users to database via command line
+ * @author    Moe Hawi <mohamadhawi@gmail.com>
 
+*/
+
+
+	/**
+     * function arguments to parse the arguments from the command line and return an array.
+    */ 
 function arguments($argv) {
 $_ARG = array();
 $seperator = array("-","--");
+
 foreach ($argv as $arg) {
          $e=explode("=",$arg);
         if(count($e)==2)
@@ -16,24 +28,49 @@ foreach ($argv as $arg) {
 return $_ARG;
 }
 
+	/**
+     * read the arguments and parse it.
+     */ 
+
 $arr = arguments($argv);
-//var_dump($argv);
+
+	/**
+     * help option.
+     */ 
 
 if (array_key_exists("help",$arr)) {
-	echo "my help goes here";
+	echo "--file [csv file name] – this is the name of the CSV to be parsed\n
+--create_table – this will cause the MySQL users table to be built (and no further action will be taken)\n
+--dry_run – this will be used with the --file directive in case we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered\n
+-u – MySQL username\n
+-p – MySQL password\n
+-h – MySQL host\n
+--help – which will output the above list of directives with details.\n";
 	exit;
 }
 
+	/**
+     * try to open a connection to database.
+     */ 
 
 
 $conn = new mysqli($arr['h'], $arr['u'], $arr['p']);
+
+	/**
+     * No connection, print error message.
+     */ 
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 	exit;
 }
-// Create database
-$sql = "CREATE DATABASE IF NOT EXISTS users";
+
+	/**
+     * create database called `dbusers`.
+	   print error if no database created.
+     */ 
+
+$sql = "CREATE DATABASE IF NOT EXISTS dbusers";
 if ($conn->query($sql) === TRUE) {
     echo "Database created successfully\n";
 } else {
@@ -42,14 +79,23 @@ if ($conn->query($sql) === TRUE) {
 }
 
 
+	/**
+     * open a connection to the database.
+	   print error if no selection successfully.
+     */ 
 
 
-$link = mysqli_connect($arr['h'], $arr['u'], $arr['p'], "users");
+$link = mysqli_connect($arr['h'], $arr['u'], $arr['p'], "dbusers");
 
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 	exit;
 }
+
+	/**
+     * create table `users`.
+	   print error if no create successfully.
+     */ 
 
 $sql = "CREATE Table IF NOT EXISTS users(
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -57,13 +103,17 @@ name VARCHAR(30) NOT NULL,
 surname VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL UNIQUE)";
 
+
 if(mysqli_query($link, $sql)){
     echo "Table created successfully.\n";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link) . "\n";
 	exit;
 }
- 
+
+	/**
+     * clear table `users` for option create_table and stop execution.
+     */ 
 if (array_key_exists("create_table",$arr)) {
 $sql = "TRUNCATE TABLE users";
 
@@ -75,6 +125,10 @@ if(mysqli_query($link, $sql)){
 exit;	
 }
 
+	/**
+     * check if parameter file was provided.
+     */
+
 if (!array_key_exists("file",$arr)) {
 	echo "ERROR: No file parameter\n";
 	exit;
@@ -84,34 +138,50 @@ if (!array_key_exists("file",$arr)) {
 $fileName = $arr["file"];
 
 
+	/**
+     * open the csv file.
+     */
 
 $file = fopen($fileName, "r");
-$message="";
 
-if (!array_key_exists("dry_run",$arr)) {
-$flag = true; 
-$dup=0;
-$r=0;
-$inv=0;
+	/**
+     * define variables for the import.
+     */
+
+
+$message=""; 
+$flag = true; // to escape the header
+$dup=0; // duplicate email counter
+$r=0; // valid rows
+$inv=0; // invalid email counter
 
 while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
 	
 	if($flag) { $flag = false; continue; }
 	
 	
-	if (filter_var( trim(addslashes($column[2])), FILTER_VALIDATE_EMAIL)) {
+	if (filter_var( trim(addslashes($column[2])), FILTER_VALIDATE_EMAIL)) { // check valid email
     $sqlInsert = "INSERT into users (name,surname,email)
 		   values ('" . ucfirst(strtolower(addslashes($column[0]))) . "','" . ucfirst(strtolower(addslashes($column[1]))) . "','" . strtolower(addslashes($column[2])) . "')";
+
+	/**
+     * if dry_run option, no insert into the DB.
+     */
+
+	if (!array_key_exists("dry_run",$arr)) {
 	$result = mysqli_query($link, $sqlInsert);
 	if ($result) $r++; else $dup++;
 	}
-	else $inv++;
+	
+	}
+	else $inv++; // invalid email
 	
 }
+	//report of data to be print.
 	$message = $r." rows data imported into the database\n".$inv." rows invalid email\n".$dup." rows duplicate email @\n" ;
 	
-}
-  // Close connection
+
+// Close connection
 $link->close();  
  
 echo $message;
